a7a0ecea1cf91981093cb32a7127616fdf8a7e6a
hypebeans <24372255+hypebeans@users.noreply.github.com>
2023-10-04 17:56:57 +0900

    Initial Commit

---
+++ PumpkinSmash/PumpkinSmashApp.swift
@@ -0,0 +1,17 @@
+//
+//  PumpkinSmashApp.swift
+//  PumpkinSmash
+//
+//  Created by Jonathan Budiman on 2023/10/04.
+//
+
+import SwiftUI
+
+@main
+struct PumpkinSmashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
