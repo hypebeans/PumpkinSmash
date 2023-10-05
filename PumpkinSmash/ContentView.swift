e8e7825f9b706beb6ca68e75e52cfd8275644cba
hypebeans <24372255+hypebeans@users.noreply.github.com>
2023-10-05 16:52:23 +0900

    Commit color json
    Added selected colors for the app

--- PumpkinSmash/ContentView.swift
+++ PumpkinSmash/ContentView.swift
@@ -7,15 +7,36 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    
+//    let primary = Color(primaryColor)
+//    let secondaryColor = Color(secondaryColor)
+//    let tertiaryColor = Color(tertiaryColor)
+//    let fourthColor = Color(fourthColor)
+//    let fifthColor = Color(fifthColor)
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Pumpkin");
+            Spacer()
+            
+            VStack {
+                Button(action: {}) {
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(.black.gradient)
+                        .frame(maxWidth: .infinity, minHeight: 30, alignment: .center)
+                        .overlay(
+                            Text("START")
+                                .font(.title)
+                                .fontWeight(.black)
+                                .foregroundColor(.white)
+                        )
+                }.padding()
+            }
+            
         }
-        .padding()
     }
 }
 
---
+++ PumpkinSmash/LevelSelectView.swift
@@ -0,0 +1,68 @@
+//
+//  LevelSelectView.swift
+//  PumpkinSmash
+//
+//  Created by Jonathan Budiman on 2023/10/04.
+//
+
+import SwiftUI
+
+struct LevelSelectView: View {
+    
+    @State var isNext = false
+    
+    
+    var body: some View {
+        VStack {
+            Text("Select Level")
+                .padding()
+            
+        
+            Button(action: {
+                .alert(isPresented: $isNext) {
+                    Alert(title: Text("CONTINUE"), message: Text("Lanjut?"), primaryButton: Button("YES"), secondaryButton: Button("NO", role: .destructive))
+                }
+            }) {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(.black.gradient)
+                    .frame(maxWidth: .infinity, minHeight: 30, alignment: .center)
+                    .overlay(
+                        Text("簡単 🤡")
+                            .font(.title)
+                            .fontWeight(.black)
+                            .foregroundColor(.white)
+                    )
+            }.padding()
+            Button(action: {}) {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(.black.gradient)
+                    .frame(maxWidth: .infinity, minHeight: 30, alignment: .center)
+                    .overlay(
+                        Text("普通 🤨")
+                            .font(.title)
+                            .fontWeight(.black)
+                            .foregroundColor(.white)
+                    )
+            }.padding()
+            Button(action: {}) {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(.black.gradient)
+                    .frame(maxWidth: .infinity, minHeight: 30, alignment: .center)
+                    .overlay(
+                        Text("難しい 🗿")
+                            .font(.title)
+                            .fontWeight(.black)
+                            .foregroundColor(.white)
+                    )
+            }.padding()
+            
+        }
+        
+    }
+}
+
+struct LevelSelectView_Previews: PreviewProvider {
+    static var previews: some View {
+        LevelSelectView()
+    }
+}
